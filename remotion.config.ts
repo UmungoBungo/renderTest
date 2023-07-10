cd31acebed25cd2b00cdd5c9f47724efa167e755
Matt McGillivray <m.mcgillivray@outlook.com>
2023-07-10 11:06:51 +0800

    repaired v4 migration

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,50 +1,16 @@
-// All configuration options: https://remotion.dev/docs/config
-// Each option also is available as a CLI flag: https://remotion.dev/docs/cli
-// ! The configuration file does only apply if you render via the CLI !
+/**
+ * Note: When using the Node.JS APIs, the config file
+ * doesn't apply. Instead, pass options directly to the APIs.
+ *
+ * All configuration options: https://remotion.dev/docs/config
+ */
 
 import {Config} from '@remotion/cli/config';
+import {enableTailwind} from '@remotion/tailwind';
 
-// Config.setVideoImageFormat('png');
+Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
 
 Config.overrideWebpackConfig((currentConfiguration) => {
-	return {
-		...currentConfiguration,
-		module: {
-			...currentConfiguration.module,
-			rules: [
-				...(currentConfiguration.module?.rules
-					? currentConfiguration.module.rules
-					: []
-				).filter((rule) => {
-					if (rule === '...') {
-						return false;
-					}
-					if (rule.test?.toString().includes('.css')) {
-						return false;
-					}
-					return true;
-				}),
-				{
-					test: /\.css$/i,
-					use: [
-						'style-loader',
-						'css-loader',
-						{
-							loader: 'postcss-loader',
-							options: {
-								postcssOptions: {
-									plugins: [
-										'postcss-preset-env',
-										'tailwindcss',
-										'autoprefixer',
-									],
-								},
-							},
-						},
-					],
-				},
-			],
-		},
-	};
+	return enableTailwind(currentConfiguration);
 });
